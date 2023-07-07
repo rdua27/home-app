865720a5d4edc863e4af7bd1f3b4c58abd9d2f5f
rdua27 <rajatdua1994@gmail.com>
2023-07-07 19:20:58 +0530

    Completed first angular official tutuorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+// import { AppComponent } from './app.component';
+// import { HomeComponent } from './home/home.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+// @NgModule({
+//   declarations: [],
+//   imports: [BrowserModule, HomeComponent],
+//   providers: [],
+//   bootstrap: [],
+// })
+// export class AppModule {}
